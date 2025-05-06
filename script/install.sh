a76b775e1aef955b75d9e0121f88e8b565d17447
Xukai Wang <kingxukai@zohomail.com>
2025-05-06 17:31:32 +0800

    add vim-gtk in install.sh
    Signed-off-by: Xukai Wang <kingxukai@zohomail.com>

--- script/install.sh
+++ script/install.sh
@@ -3,8 +3,8 @@
 set -xe
 
 sudo apt update
-sudo apt install trash-cli htop vim glow tmux ghostty
-sudo apt upgrade trash-cli htop vim glow tmux ghostty
+sudo apt install trash-cli htop vim vim-gtk glow tmux ghostty
+sudo apt upgrade trash-cli htop vim vim-gtk glow tmux ghostty
 eval "$(wget -O- https://get.x-cmd.com)"
 x install lla
 x install yazi
